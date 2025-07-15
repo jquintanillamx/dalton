a962aa37c1aa0e4a7bf029bd741381e3519887fb
Jorge <jquintanilla901@gmail.com>
2025-07-15 02:36:27 -0600

    11build

--- src/App.jsx
+++ src/App.jsx
@@ -84,7 +84,7 @@ const App = () => {
     const loadFacebookPost = async () => {
       if (conversationChannel === "FB" && conversationUrl) {
         try {
-          const response = await fetch(conversationUrl, { credentials: "include" });
+          const response = await fetch(conversationUrl);
           const htmlText = await response.text();
           const parser = new DOMParser();
           const doc = parser.parseFromString(htmlText, "text/html");
